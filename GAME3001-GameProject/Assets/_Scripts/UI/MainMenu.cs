649eec02e0f88fdb532518415ad4f47f7997ff3d
aytona <christopher.aytona@gmail.com>
2015-12-05 20:43:06 -0500

    Initial Commit

---
+++ GAME3001-GameProject/Assets/_Scripts/UI/MainMenu.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+    public GameObject HowTo;
+    public GameObject Menu;
+
+    public void doStart()
+    {
+        Application.LoadLevel(1);
+    }
+
+    public void doHowTo()
+    {
+        Menu.SetActive(false);
+        HowTo.SetActive(true);
+    }
+
+    public void doMenu()
+    {
+        Menu.SetActive(true);
+        HowTo.SetActive(false);
+    }
+
+    public void doQuit()
+    {
+        Application.Quit();
+    }
+}
