6be154c152234d7c044551809d60bfc7b7f65fcf
aytona <christopher.aytona@gmail.com>
2015-12-06 21:28:13 -0500

    wave spawner

---
+++ Assets/_Scripts/Enemy/EnemySpawner.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+
+    public GameObject enemy;
+    public Vector3 spawnLocation;
+    public int enemyCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+
+    private GameObject player;
+
+    void Start()
+    {
+        StartCoroutine(SpawnWaves());
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void OnLevelWasLoaded()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+
+        while (true)
+        {
+            for (int i = 0; i < enemyCount; i++)
+            {
+                Vector3 spawnPosition = new Vector3(spawnLocation.x, spawnLocation.y, Random.Range(-spawnLocation.z, spawnLocation.z)-20);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(enemy, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+            if (player == null)
+                break;
+        }
+    }
+}
