0b00f458809e3a41bd682a837533c4dd0bcb290d
aytona <christopher.aytona@gmail.com>
2015-12-13 17:01:51 -0500

    github late update, credits added

--- Assets/_Scripts/UI/MainMenu.cs
+++ Assets/_Scripts/UI/MainMenu.cs
@@ -4,22 +4,33 @@ public class MainMenu : MonoBehaviour {
 
     public GameObject HowTo;
     public GameObject Menu;
+    public GameObject Credits;
 
     public void doStart()
     {
         Application.LoadLevel(1);
     }
 
+    // Going from menu to how to
     public void doHowTo()
     {
         Menu.SetActive(false);
         HowTo.SetActive(true);
     }
 
+    // Going from menu to credits
+    public void doCredits()
+    {
+        Menu.SetActive(false);
+        Credits.SetActive(true);
+    }
+
+    // Going back to main menu
     public void doMenu()
     {
         Menu.SetActive(true);
         HowTo.SetActive(false);
+        Credits.SetActive(false);
     }
 
     public void doQuit()
